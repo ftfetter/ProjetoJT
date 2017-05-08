082ae1001e0c889d0dd3d22334fb5716db020b83
Felipe Fetter <felipetfetter@gmail.com>
2017-05-08 18:44:40 -0300

    Todos os testes passando

--- src/test/java/View/LoginFrameTest.java
+++ src/test/java/View/LoginFrameTest.java
@@ -4,12 +4,17 @@ import Beans.Usuario;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 public class LoginFrameTest {
     Usuario usuario = new Usuario("admin","admin");
-    LoginFrame loginFrame = new LoginFrame();
+    LoginFrame loginFrame = mock(LoginFrame.class);
 
     @Test
     public void validandoLogin (){
+
+        when(loginFrame.validarLogin(usuario)).thenReturn(true);
+
         Assert.assertTrue(loginFrame.validarLogin(usuario));
     }
 
@@ -17,12 +22,16 @@ public class LoginFrameTest {
     public void logandoTreinador (){
         usuario.setTipoLogin(1);
 
+        when(loginFrame.login(usuario)).thenReturn(true);
+
         Assert.assertTrue(loginFrame.login(usuario));
     }
 
     @Test
     public void logandoAluno (){
         usuario.setTipoLogin(2);
+
+        when(loginFrame.login(usuario)).thenReturn(true);
 
         Assert.assertTrue(loginFrame.login(usuario));
     }
