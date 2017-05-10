1d93d5d7e64d4f5b9a40450437ac148496f128b7
Felipe Fetter <felipetfetter@gmail.com>
2017-05-10 18:28:11 -0300

    Alterações no UsuarioDAO

--- src/main/java/Database/UsuarioDAO.java
+++ src/main/java/Database/UsuarioDAO.java
@@ -13,7 +13,7 @@ public class UsuarioDAO {
     public boolean adicionarUsuario(Aluno aluno){
 
         boolean retorno = false;
-        String insert = "INSERT INTO usuario(usuario, senha, tipo, aluno_id) VALUES(?, ?, 2, ?);";
+        String insert = "INSERT INTO usuario(usuario, senha, tipo, usuario_id) VALUES(?, ?, 2, ?);";
 
         try {
             connection = DatabaseConnect.getConnection();
@@ -23,6 +23,42 @@ public class UsuarioDAO {
             preparedStatement.setString(1,aluno.getLogin());
             preparedStatement.setString(2,aluno.getSenha());
             preparedStatement.setInt(3,aluno.getId());
+
+            if(preparedStatement.executeUpdate()>0)
+                retorno = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return retorno;
+    }
+
+    public boolean adicionarUsuario(Treinador treinador){
+
+        boolean retorno = false;
+        String insert = "INSERT INTO usuario(usuario, senha, tipo, usuario_id) VALUES(?, ?, 1, ?);";
+
+        try {
+            connection = DatabaseConnect.getConnection();
+            //preparando o INSERT
+            preparedStatement = connection.prepareStatement(insert);
+            //setando as variáveis
+            preparedStatement.setString(1,treinador.getLogin());
+            preparedStatement.setString(2,treinador.getSenha());
+            preparedStatement.setInt(3,treinador.getId());
 
             if(preparedStatement.executeUpdate()>0)
                 retorno = true;
@@ -61,15 +97,8 @@ public class UsuarioDAO {
 
             resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
-
                 usuario.setTipoLogin(resultSet.getInt(4));
-
-                if(usuario.getTipoLogin() == 1) {
-                    usuario.setId(resultSet.getInt(5));
-
-                } else {
-                    usuario.setId(resultSet.getInt(6));
-                }
+                usuario.setId(resultSet.getInt(5));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -93,7 +122,7 @@ public class UsuarioDAO {
 
     public boolean excluirUsuario(Aluno aluno){
         Boolean retorno = false;
-        String delete = "DELETE FROM usuario WHERE aluno_id = ?;";
+        String delete = "DELETE FROM usuario WHERE usuario_id = ?;";
 
         try{
             connection = DatabaseConnect.getConnection();
