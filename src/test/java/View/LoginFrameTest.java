bccd6c6c9b0c96ed67ec76423c60a813eac66676
Felipe Fetter <felipetfetter@gmail.com>
2017-05-11 18:57:53 -0300

    Remoção de testes não funcionais

--- src/test/java/View/LoginFrameTest.java
+++ src/test/java/View/LoginFrameTest.java
@@ -1,5 +1,6 @@
 package View;
 
+import Beans.Treinador;
 import Beans.Usuario;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,32 +8,11 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class LoginFrameTest {
-    Usuario usuario = new Usuario("admin","admin");
-    LoginFrame loginFrame = mock(LoginFrame.class);
+    Usuario usuario = new Usuario("login","senha");
+    LoginFrame loginFrame = new LoginFrame();
 
     @Test
     public void validandoLogin (){
-
-        when(loginFrame.validarLogin(usuario)).thenReturn(true);
-
         Assert.assertTrue(loginFrame.validarLogin(usuario));
-    }
-
-    @Test
-    public void logandoTreinador (){
-        usuario.setTipoLogin(1);
-
-        when(loginFrame.login(usuario)).thenReturn(true);
-
-        Assert.assertTrue(loginFrame.login(usuario));
-    }
-
-    @Test
-    public void logandoAluno (){
-        usuario.setTipoLogin(2);
-
-        when(loginFrame.login(usuario)).thenReturn(true);
-
-        Assert.assertTrue(loginFrame.login(usuario));
     }
 }
