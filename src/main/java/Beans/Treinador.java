1d0305392628860d81bb1613ea5c81bf3c49a0de
Felipe Fetter <felipetfetter@gmail.com>
2017-05-11 18:13:07 -0300

    Limpeza de código e melhorias na organização

--- src/main/java/Beans/Treinador.java
+++ src/main/java/Beans/Treinador.java
@@ -6,6 +6,11 @@ public class Treinador extends Usuario {
 
     }
 
+    public Treinador(int id, String nome){
+        super.setId(id);
+        super.setNome(nome);
+    }
+
     public Treinador(int id, String nome, String cpf, int tipoLogin, String login, String senha) {
         super(login, senha);
         super.setId(id);
